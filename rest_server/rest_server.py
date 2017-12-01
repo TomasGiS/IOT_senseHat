7e988857e504a014039d1ff3f2700374f20a61ea
TomasGiS <me@tomasgis.cat>
2017-12-02 00:20:52 +0100

    Basic Rest server with all sensor reading

--- rest_server/rest_server.py
+++ rest_server/rest_server.py
@@ -4,11 +4,24 @@ import json
 urls = (
 	'/temperature', 'temperatureHAT',
 	'/humidity', 'humidityHAT',
-	'/pressure', 'pressureHAT'
+	'/pressure', 'pressureHAT',
+	'/all_sensors','sensorsHAT'
 )
 
 sense = SenseHat()
 app = web.application(urls, globals())
+
+class sensorsHAT:
+	def GET(self):
+		temperature = sense.temperature
+                jsonStrTemp={"sensor": "temperature","value":temperature}
+		humidity = sense.humidity
+                jsonStrHumidity={"sensor": "humidity","value":humidity}
+		pressure = sense.pressure
+                jsonStrPressure={"sensor":"pressure","value":pressure}
+
+		allSensors={"code":200,"status":"ok","sensor":"all","value":[jsonStrTemp,jsonStrHumidity,jsonStrPressure]}
+		return json.dumps(allSensors)
 
 class temperatureHAT:
 	def GET(self):
